0cd028665f0b25cd4c21e4f4989b002aacd78098
srect <fcqun@sina.cn>
2022-05-11 21:16:27 +0800

    feat: Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// https://flutter.cn/docs/get-started/codelab
 void main() {
   runApp(const MyApp());
 }
@@ -14,7 +15,18 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           appBar: AppBar(title: const Text('giao giao')),
           body: const Center(
-            child: Text('hello'),
+            child: Text(
+              'hello flutter yi giao wo di giao wo yao feng kuang guai bao bao yi giao wo di giao',
+              textAlign: TextAlign.left,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Color.fromARGB(255, 222, 103, 6),
+                  decoration: TextDecoration.underline,
+                  decorationColor: Color.fromARGB(255, 55, 20, 136),
+                  decorationStyle: TextDecorationStyle.dashed),
+            ),
           ),
         ));
   }
