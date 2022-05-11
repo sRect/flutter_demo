8834f488022b158328c1199bc3441f8be6f7a6d8
srect <fcqun@sina.cn>
2022-05-11 18:17:45 +0800

    feat: init

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'welcome flutter',
+        home: Scaffold(
+          appBar: AppBar(title: const Text('giao giao')),
+          body: const Center(
+            child: Text('hello'),
+          ),
+        ));
+  }
+}
